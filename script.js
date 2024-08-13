744604d91c29b1854cd52283c9d8d569a89f2963
Oni Abdullahi Oluwabusayo <busayaooni@gmail.com>
2024-08-13 10:51:24 +0100

    latest version

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ var divide = document.querySelector(".divide");
 var percentage = document.querySelector(".percentage");
 var multiply = document.querySelector(".multiply");
 var equal = document.querySelector(".equal");
+var square = document.querySelector('.square');
+var Expo = document.querySelector('.Expo');
+var log = document.querySelector('.log');
+var ln = document.querySelector('.ln');
+var sin = document.querySelector('.sin');
+var cos = document.querySelector('.cos');
+var tan = document.querySelector('.tan');
+var asin = document.querySelector('.asin');
+var acos = document.querySelector('.acos');
+var atan = document.querySelector('.atan');
 var sum = 0;
 
 
@@ -46,92 +56,79 @@ document.querySelector('.multiply').onclick = function() {
     sum = value1 * value2;
 }
 
+document.querySelector('.square').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.sqrt(value1);
+}
 
-function getInputValues() {
-    const value1 = parseFloat(document.getElementById('inputValue1').value);
-    const value2 = parseFloat(document.getElementById('inputValue2').value);
-    sum = [value1, value2];
+document.querySelector('.Expo').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.exp(value1);
+}
+
+document.querySelector('.log').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.log10(value1);
+}
+
+document.querySelector('.ln').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.log(value1);
+}
+
+document.querySelector('.sin').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.sin(value1);
+}
+
+document.querySelector('.cos').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.cos(value1);
 }
 
 
+document.querySelector('.tan').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.tan(value1);
+}
+
+
+document.querySelector('.asin').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.asin(value1);
+}
+
+
+document.querySelector('.acos').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.acos(value1);
+}
+
+
+document.querySelector('.atan').onclick = function() {
+    let value1 = parseFloat(document.querySelector('.firstvalue').value);
+    let value2 = parseFloat(document.querySelector('.secondvalue').value);
+    sum = Math.atan(value1);
+}
+
+
+
+
 document.querySelector('.equal').onclick = function() {
-    // output.innerText = 45
     document.querySelector('.output').innerText = `${sum}`;
 
 
     document.querySelector('.firstvalue').value = '';
     document.querySelector('.secondvalue').value = '';
 
-        document.getElementById('sum').textContent = 'sum: ' + result;
+        // document.getElementById('sum').textContent = 'sum: ' + result;
 };
-
-
-// Function to calculate square root
-function calculateSquareRoot() {
-    const [value1] = getInputValues();
-    const result = Math.sqrt(value1);
-    displayResult(result);
-}
-
-// Function to calculate exponential (e^x)
-function calculateExponential() {
-    const [value1] = getInputValues();
-    const result = Math.exp(value1);
-    displayResult(result);
-}
-
-// Function to calculate logarithm (log base 10)
-function calculateLog() {
-    const [value1] = getInputValues();
-    const result = Math.log10(value1);
-    displayResult(result);
-}
-
-// Function to calculate natural logarithm (ln)
-function calculateLn() {
-    const [value1] = getInputValues();
-    const result = Math.log(value1);
-    displayResult(result);
-}
-
-// Function to calculate sine
-function calculateSin() {
-    const [value1] = getInputValues();
-    const result = Math.sin(value1);
-    displayResult(result);
-}
-
-// Function to calculate cosine
-function calculateCos() {
-    const [value1] = getInputValues();
-    const result = Math.cos(value1);
-    displayResult(result);
-}
-
-// Function to calculate tangent
-function calculateTan() {
-    const [value1] = getInputValues();
-    const result = Math.tan(value1);
-    displayResult(result);
-}
-
-// Function to calculate arcsine
-function calculateAsin() {
-    const [value1] = getInputValues();
-    const result = Math.asin(value1);
-    displayResult(result);
-}
-
-// Function to calculate arccosine
-function calculateAcos() {
-    const [value1] = getInputValues();
-    const result = Math.acos(value1);
-    displayResult(result);
-}
-
-// Function to calculate arctangent
-function calculateAtan() {
-    const [value1] = getInputValues();
-    const result = Math.atan(value1);
-    displayResult(result);
-}
